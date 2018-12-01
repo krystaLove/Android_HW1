9c223bde677e765af9c2e43cbaca732d41f6bd60
Денис <denis.zhuravkov@gmail.com>
2018-12-01 17:17:40 +0200

    upload

---
+++ app/src/main/java/com/krystalove/testcalc/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.krystalove.testcalc
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.view.View
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.Math.round
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun calculate(v: View) {
+        if(firstET.text.isEmpty()||secondET.text.toString().isEmpty())
+            Snackbar.make(findViewById(R.id.root_layout),R.string.snackbar_message_1,Snackbar.LENGTH_LONG).show()
+        else {
+            val op = (v as Button).text.toString()
+            val firstNumb = firstET.text.toString().toDouble()
+            val secondNumb = secondET.text.toString().toDouble()
+            val result: Double = when (op) {
+                "+" -> firstNumb + secondNumb
+                "-" -> firstNumb - secondNumb
+                "*" -> firstNumb * secondNumb
+                "/" -> firstNumb / secondNumb
+                else -> 0.0
+            }
+            if (result.toString() == "Infinity")
+                Snackbar.make(findViewById(R.id.root_layout), R.string.snackbar_message_2, Snackbar.LENGTH_LONG).show()
+            else
+                resultTW.text = result.toString()
+        }
+    }
+}
